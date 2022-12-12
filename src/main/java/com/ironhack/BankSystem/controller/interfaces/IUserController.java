3e2d576621ad2c3bae659a7b4fdafe9928b08f71
Ariana7 <angela07rosales@gmail.com>
2022-12-12 05:43:42 +0100

    t

--- src/main/java/com/ironhack/BankSystem/controller/interfaces/IUserController.java
+++ src/main/java/com/ironhack/BankSystem/controller/interfaces/IUserController.java
@@ -12,36 +12,27 @@ import com.ironhack.BankSystem.model.User;
 import java.util.List;
 
 public interface IUserController {
-    // ======================================== GET Methods ========================================
-    // -------------------- Global USER [ADMIN] --------------------
+
     List<User> getUsers();
 
     User getUserById(long id);
 
     User getUserByUsername(String username);
 
-    // -------------------- Admins [ADMIN] --------------------
     List<Admin> getAdmins();
 
-    // -------------------- Account Holder [ADMIN] --------------------
     List<AccountHolder> getAccountHolders();
 
-    // -------------------- Third Parties [ADMIN] --------------------
     List<ThirdParty> getAThirdParties();
 
 
-    // ======================================== POST Methods ========================================
-    // -------------------- New Admin [ADMIN] --------------------
     void createNewAdmin(UserDTO admin);
 
-    // -------------------- New Account Holder [PUBLIC] --------------------
     void createNewAccountHolder(UserAccountHolderDTO accountHolder);
 
-    // -------------------- New Third Party [ADMIN] --------------------
     void createNewThirdParty(UserDTO thirdParty);
 
 
-    // ======================================== PATCH Methods ========================================
     void editPassword(String username, UserEditPasswordDTO userPassword);
 
     void editUser(String username, UserEditDTO user);
